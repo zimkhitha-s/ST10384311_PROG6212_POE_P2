88d54904f08e091bb2a6fa09c7a048573866ddab
Zimkhitha Sasanti <130903306+ZimkhtihaOlgaSasanti@users.noreply.github.com>
2024-09-06 10:38:06 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10384311PROG6212POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
