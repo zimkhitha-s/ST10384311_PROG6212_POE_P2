4e671ca1e31d48faba15f936be876a3f88194bb3
Zimkhitha Sasanti <130903306+ZimkhtihaOlgaSasanti@users.noreply.github.com>
2024-09-10 16:51:49 +0200

    Updated the Pages and style.

---
+++ wwwroot/js/submitclaims.js
@@ -0,0 +1,35 @@
+﻿/*The Javascript Code for the Submit Claims Page*/
+
+// Choosing between full-time or part-time lecturer
+document.addEventListener("DOMContentLoaded", function () {
+    const lecturerType = document.getElementById('lecturerType');
+    const claimOptionsFullTime = document.getElementById('claimOptionsFullTime');
+    const claimOptionsPartTime = document.getElementById('claimOptionsPartTime');
+
+    lecturerType.addEventListener('change', function () {
+        if (lecturerType.value === 'full-time') {
+            claimOptionsFullTime.style.display = 'block';
+            claimOptionsPartTime.style.display = 'none';
+        } else if (lecturerType.value === 'part-time') {
+            claimOptionsFullTime.style.display = 'none';
+            claimOptionsPartTime.style.display = 'block';
+        } else {
+            claimOptionsFullTime.style.display = 'none';
+            claimOptionsPartTime.style.display = 'none';
+        }
+    });
+});
+
+// Choosing between the different claim types
+document.getElementById('claimTypePartTime').addEventListener('change', function () {
+    var salaryFields = document.getElementById('salaryFields');
+
+    // Check if the selected claim type is 'Lecture Hours'
+    if (this.value === 'lecture-hours') {
+        salaryFields.style.display = 'block';  // Show salary fields
+    } else {
+        salaryFields.style.display = 'none';   // Hide salary fields
+    }
+});
+
+
